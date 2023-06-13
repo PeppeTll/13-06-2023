9ab919ab035afb7d81cde96b2973275a6f7aa323
PeppeTll <peppetll@gmail.com>
2023-06-13 16:05:46 +0200

    mobile first

---
+++ src/componets/todo/Todo.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+import "./index.css";
+
+const Todo = ({ todo }) => {
+	return (
+		<div className="todo">
+			<i
+				className={
+					todo.completed ? "fa-solid fa-check-double" : "fa-solid fa-clipboard"
+				}
+			></i>
+			<p className="todo__p">{todo.todo}</p>
+		</div>
+	);
+};
+
+export default Todo;
